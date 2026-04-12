bc06cf82d21ddeb832596a2c648f36b5759450cb
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2026-04-12 23:11:57 +0200

    #Solve Cors Error

--- Backend/src/SmartGuide/API/Program.cs
+++ Backend/src/SmartGuide/API/Program.cs
@@ -1,3 +1,67 @@
+//using API.Extentions;
+//using Application.Services.Interfaces;
+//using Application.Settings;
+//using Infrastructure.Data;
+//using Infrastructure.Services.Email;
+//using Infrastructure.Settings;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowFrontend", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:5174")
+//              .AllowAnyMethod()
+//              .AllowAnyHeader()
+//              .AllowCredentials();
+//    });
+//});
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+
+////add Application , Infrastructure registration
+//builder.Services.InfrastructureConfiguration(builder.Configuration);
+//builder.Services.ApplicationConfiguration(builder.Configuration);
+
+////builder.Services.AddScoped<IEmailService, EmailService>();
+
+
+
+//builder.Services.AddControllers();
+//// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+//var app = builder.Build();
+
+
+
+//await app.MigrateDataBaseAsync();
+
+
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+//app.UseCors("AllowFrontend"); 
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
 using API.Extentions;
 using Application.Services.Interfaces;
 using Application.Settings;
@@ -6,10 +70,11 @@ using Infrastructure.Services.Email;
 using Infrastructure.Settings;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//  CORS CONFIG 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
@@ -20,32 +85,39 @@ builder.Services.AddCors(options =>
               .AllowCredentials();
     });
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.MaxRequestBodySize = 52428800; 
+});
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 52428800;
+});
+
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 52428800;
+});
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-//add Application , Infrastructure registration
+// Application + Infrastructure
 builder.Services.InfrastructureConfiguration(builder.Configuration);
 builder.Services.ApplicationConfiguration(builder.Configuration);
 
-//builder.Services.AddScoped<IEmailService, EmailService>();
-
-
-
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-
-
 await app.MigrateDataBaseAsync();
 
-
-
-// Configure the HTTP request pipeline.
+// Swagger
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -53,7 +125,24 @@ if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseCors("AllowFrontend"); 
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:5174");
+        context.Response.Headers.Add("Access-Control-Allow-Headers", "*");
+        context.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+        context.Response.StatusCode = 200;
+        await context.Response.CompleteAsync();
+        return;
+    }
+
+    await next();
+});
+
+app.UseCors("AllowFrontend");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
