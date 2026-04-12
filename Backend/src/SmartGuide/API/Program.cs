81da4e5a992b46b2fc1f16c69e30b0df1b019661
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2026-04-12 22:48:49 +0200

    #Allow Cors

--- Backend/src/SmartGuide/API/Program.cs
+++ Backend/src/SmartGuide/API/Program.cs
@@ -12,14 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins("http://localhost:5174")
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowAnyHeader()
+              .AllowCredentials();
     });
 });
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -53,7 +53,7 @@ if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseCors();
+app.UseCors("AllowFrontend"); 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
