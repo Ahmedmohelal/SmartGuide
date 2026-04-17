71838d02aee30f5277ee49986540165c5f675244
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2026-04-17 02:46:59 +0200

    #Add ClientID

--- Backend/src/SmartGuide/API/Program.cs
+++ Backend/src/SmartGuide/API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 
 //add Application , Infrastructure registration
