794f9ebfd14f4de83e59747eea6ede87643003cc
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2026-04-17 03:11:36 +0200

    #Uncomment Swagger

--- Backend/src/SmartGuide/API/Program.cs
+++ Backend/src/SmartGuide/API/Program.cs
@@ -22,14 +22,13 @@ builder.Services.AddCors(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 
 //add Application , Infrastructure registration
 builder.Services.InfrastructureConfiguration(builder.Configuration);
 builder.Services.ApplicationConfiguration(builder.Configuration);
 
-//builder.Services.AddScoped<IEmailService, EmailService>();
 
 
 
