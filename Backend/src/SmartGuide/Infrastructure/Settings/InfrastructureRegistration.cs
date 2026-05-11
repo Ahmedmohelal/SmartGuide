ef6e2d9d5381c7012d0551dfae80f6fbafbe5ffa
A7madMedhat <abrasha159357@gmail.com>
2026-05-11 06:31:09 +0300

    Change The Connection String

--- Backend/src/SmartGuide/Infrastructure/Settings/InfrastructureRegistration.cs
+++ Backend/src/SmartGuide/Infrastructure/Settings/InfrastructureRegistration.cs
@@ -42,7 +42,7 @@ namespace Infrastructure.Settings
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    configuration.GetConnectionString("AhmedDefaultConnection"),
+                    configuration.GetConnectionString("DefaultConnection"),
                         sqlOptions => sqlOptions.EnableRetryOnFailure()
             ));
 
