c43c504b7e1ff47c7a4dc0690a03c26866b8f2f7
A7madMedhat <abrasha159357@gmail.com>
2026-06-19 23:20:07 +0300

    Edit Conn String

--- Backend/src/SmartGuide/Infrastructure/Settings/InfrastructureRegistration.cs
+++ Backend/src/SmartGuide/Infrastructure/Settings/InfrastructureRegistration.cs
@@ -55,7 +55,7 @@ namespace Infrastructure.Settings
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    configuration.GetConnectionString("AhmedDefaultConnection"),
+                    configuration.GetConnectionString("DefaultConnection"),
                         sqlOptions => sqlOptions.EnableRetryOnFailure()
             ));
 
